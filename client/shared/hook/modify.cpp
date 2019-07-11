7272d3888a2cbe1bcb62a6a43620748e7db6401b
Almog Hamdani <almogh52@gmail.com>
2019-07-11 03:10:22 +0300

    Add more utils to the modify class

--- client/shared/hook/modify.cpp
+++ client/shared/hook/modify.cpp
@@ -4,7 +4,8 @@
 
 void gtamp::hook::modify::put(void *dst, uint8_t opcode, uint64_t amount)
 {
-	for (int i = 0; i < amount; i++) {
+	for (int i = 0; i < amount; i++)
+	{
 		*((uint8_t *)dst + i) = opcode;
 	}
 }
@@ -12,4 +13,33 @@ void gtamp::hook::modify::put(void *dst, uint8_t opcode, uint64_t amount)
 void gtamp::hook::modify::put(void *dst, uint8_t opcode)
 {
 	*(uint8_t *)dst = opcode;
+}
+
+void gtamp::hook::modify::put(void *dst, std::vector<uint8_t> vec)
+{
+	for (auto iterator = vec.begin(); iterator != vec.end(); iterator++)
+	{
+		*((uint8_t *)dst + (vec.end() - iterator)) = *iterator;
+	}
+}
+
+void gtamp::hook::modify::nop(void *dst)
+{
+	put(dst, 0x90);
+}
+
+void gtamp::hook::modify::nop(void *dst, uint64_t amount)
+{
+	put(dst, 0x90, amount);
+}
+
+void gtamp::hook::modify::ret(void *dst, uint8_t params_size = 0)
+{
+	if (!params_size)
+	{
+		put(dst, 0xC3);
+	} else {
+		put(dst, 0xC2);
+		put((char *)dst + 1, params_size);
+	}
 }
--- client/shared/hook/modify.h
+++ client/shared/hook/modify.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <vector>
 
 namespace gtamp
 {
@@ -11,6 +12,12 @@ class modify
 public:
 	static void put(void *dst, uint8_t opcode, uint64_t amount);
 	static void put(void *dst, uint8_t opcode);
+	static void put(void *dst, std::vector<uint8_t> vec);
+
+	static void nop(void *dst);
+	static void nop(void *dst, uint64_t amount);
+
+	static void ret(void *dst, uint8_t params_size = 0);
 };
 }; // namespace hook
 }; // namespace gtamp
